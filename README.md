5352a62c7207d2b22182027d032b64dae06075ef
Steve Shead <steve@steve-shead.com>
2019-05-19 16:23:25 -0700

    initial commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bootstrap4-monome
+# Mono Me - Bootstrap 4 HTML Theme
+
+Mono Me - Bootstrap 4 HTML Theme
+
+View the <a href="https://steveshead.github.io/bootstrap4-monome/">demo</a> site <a href="https://steveshead.github.io/bootstrap4-monome/">here</a>.
+
+If you end up using this or any part of it please attribute it to me, Steve Shead, http://www.steve-shead.com.
+
+Thanks!
---
+++ assets/js/scripts.js
@@ -0,0 +1,97 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+// Smooth scroll with scrollspy
+$(document).ready(function() {
+$(".nav-link,.dropdown-item").click(function() {
+    var t = $(this).attr('href');
+    $('.active').not('.carousel-item.active').removeClass('active');
+    $("html, body").animate({
+        scrollTop: $(t).offset().top - 50
+    }, {
+        duration: 1e3,
+    });
+
+    $('body').scrollspy({ target: '#navbar-example',offset: $(t).offset().top });
+    $(this).parent().addClass('active');
+
+    return false;
+});
+
+});
+
+// Typewriter
+//made by vipul mirajkar thevipulm.appspot.com
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #333}";
+        document.body.appendChild(css);
+    };
